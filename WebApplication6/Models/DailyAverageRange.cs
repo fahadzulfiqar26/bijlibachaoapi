62ddcfa41d3db492f0753484102beec080d36a2d
fahadzulfiqar26 <41937719+fahadzulfiqar26@users.noreply.github.com>
2023-10-17 21:47:32 +0500

    commit

---
+++ WebApplication6/Models/DailyAverageRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication6.Models
+{
+    public class DailyAverageRange
+    {
+        public String Id { get; set; }
+        public String Type { get; set; } // L1,L2,L3
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; } 
+
+    }
+}
